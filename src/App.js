59fe36a4e8017d2dc86d93b0e4484478356bee13
Deyvid William <deyvidwms@gmail.com>
2022-07-26 19:47:10 -0300

    refatoração listagem de nome de times

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ function App() {
   return (
     <div className="App">
       <Banner />
-      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
+      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
 
       {times.map(time => <Time key={time.id} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria} />)}
 
