f0daa2d83360e72723acfbef05435495acf409ba
Deyvid William <deyvidwms@gmail.com>
2022-07-26 19:58:29 -0300

    Criação do componente colaborador

---
+++ src/components/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+  return (
+    <div className='colaborador'>
+      <div className='cabecalho'>
+        <img src="https://github.com/deyvidwms.png" alt="Deyvid William" />
+      </div>
+      <div className='rodape'>
+        <h4>Deyvid William</h4>
+        <h5>Instrutor</h5>
+      </div>
+    </div>
+  )
+}
+
+export default Colaborador
