545cd517e344abe534b5f08d10e8d70f7dfd56d7
Deyvid William <deyvidwms@gmail.com>
2022-07-25 21:53:37 -0300

    Iniciando os eventos de onSubmit

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -7,7 +7,7 @@ const CampoTexto = (props) => {
   return (
     <div className="campo-texto">
       <label>{props.label}</label>
-      <input type="text" placeholder={placeholderModificada} />
+      <input required={props.obrigatorio} type="text" placeholder={placeholderModificada} />
     </div>
   )
 }
